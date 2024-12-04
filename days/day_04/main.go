bd47881c634b3c12c29a749872742f38cd7421e7
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-12-04 09:25:42 +0100

    part 1

--- days/day_04/main.go
+++ days/day_04/main.go
@@ -2,7 +2,25 @@ package day_04
 
 import (
 	"fmt"
+	"github.com/wlchs/aoc24/types"
+	"github.com/wlchs/aoc24/utils"
+	"strconv"
 )
+
+// search recursively moves in the provided direction and tries to match the given string.
+// If a match is found, the result is 1, otherwise 0
+func search(m map[types.Vec2]int32, pos types.Vec2, s string, dir types.Vec2) int {
+	if s == "" {
+		return 1
+	}
+
+	head := int32(s[0])
+	if m[pos] != head {
+		return 0
+	}
+
+	return search(m, pos.Add(&dir), s[1:], dir)
+}
 
 // Run function of the daily challenge
 func Run(input []string, mode int) {
@@ -16,7 +34,24 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	m := utils.ParseInputToMap(input)
+	count := 0
+
+	for y := range input {
+		for x := range input[y] {
+			head := types.Vec2{X: x, Y: y}
+			count += search(m, head, "XMAS", types.Vec2{X: 0, Y: -1})
+			count += search(m, head, "XMAS", types.Vec2{X: 1, Y: -1})
+			count += search(m, head, "XMAS", types.Vec2{X: 1, Y: 0})
+			count += search(m, head, "XMAS", types.Vec2{X: 1, Y: 1})
+			count += search(m, head, "XMAS", types.Vec2{X: 0, Y: 1})
+			count += search(m, head, "XMAS", types.Vec2{X: -1, Y: 1})
+			count += search(m, head, "XMAS", types.Vec2{X: -1, Y: 0})
+			count += search(m, head, "XMAS", types.Vec2{X: -1, Y: -1})
+		}
+	}
+
+	return strconv.Itoa(count)
 }
 
 // Part2 solves the second part of the exercise
