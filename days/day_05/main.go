d99e13e6b90dafc29764dc0daca983ea93439ae9
László Borbély <laszlo.borbely@mgm-tp.com>
2024-12-05 11:02:43 +0100

    Day 5 (#6)
    * part 1
    
    * part 2

--- days/day_05/main.go
+++ days/day_05/main.go
@@ -2,7 +2,25 @@ package day_05
 
 import (
 	"fmt"
+	"github.com/wlchs/aoc24/types"
+	"github.com/wlchs/aoc24/utils"
+	"slices"
+	"strconv"
+	"strings"
 )
+
+// checkAndCorrectInput tests whether the input is valid and if not, tries to correct it
+func checkAndCorrectInput(values []int, constraints []types.Vec2) {
+	for y := 0; y < len(values); y++ {
+		for x := y + 1; x < len(values); x++ {
+			if slices.Contains(constraints, types.Vec2{X: values[x], Y: values[y]}) {
+				tmp := values[y]
+				values[y] = values[x]
+				values[x] = tmp
+			}
+		}
+	}
+}
 
 // Run function of the daily challenge
 func Run(input []string, mode int) {
@@ -16,10 +34,50 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	emptyLineId := slices.Index(input, "")
+	constraints := make([]types.Vec2, 0, emptyLineId)
+	sum := 0
+
+	for _, row := range input[:emptyLineId] {
+		values := utils.ToIntSlice(strings.Split(row, "|"))
+		constraints = append(constraints, types.Vec2{X: values[0], Y: values[1]})
+	}
+
+	for _, row := range input[emptyLineId+1:] {
+		values := utils.ToIntSlice(strings.Split(row, ","))
+		clone := slices.Clone(values)
+
+		checkAndCorrectInput(clone, constraints)
+
+		if slices.Compare(values, clone) == 0 {
+			sum += clone[(len(clone)-1)/2]
+		}
+	}
+
+	return strconv.Itoa(sum)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	emptyLineId := slices.Index(input, "")
+	constraints := make([]types.Vec2, 0, emptyLineId)
+	sum := 0
+
+	for _, row := range input[:emptyLineId] {
+		values := utils.ToIntSlice(strings.Split(row, "|"))
+		constraints = append(constraints, types.Vec2{X: values[0], Y: values[1]})
+	}
+
+	for _, row := range input[emptyLineId+1:] {
+		values := utils.ToIntSlice(strings.Split(row, ","))
+		clone := slices.Clone(values)
+
+		checkAndCorrectInput(clone, constraints)
+
+		if slices.Compare(values, clone) != 0 {
+			sum += clone[(len(clone)-1)/2]
+		}
+	}
+
+	return strconv.Itoa(sum)
 }
