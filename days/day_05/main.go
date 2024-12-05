31e1fd8cedee7dd9075f84f1324bd3ef5a3ba11a
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-12-05 10:36:15 +0100

    part 1

--- days/day_05/main.go
+++ days/day_05/main.go
@@ -2,6 +2,11 @@ package day_05
 
 import (
 	"fmt"
+	"github.com/wlchs/aoc24/types"
+	"github.com/wlchs/aoc24/utils"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 // Run function of the daily challenge
@@ -16,7 +21,33 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	emptyLineId := slices.Index(input, "")
+	constraints := make([]types.Vec2, 0, emptyLineId)
+	sum := 0
+
+	for _, row := range input[:emptyLineId] {
+		values := utils.ToIntSlice(strings.Split(row, "|"))
+		constraints = append(constraints, types.Vec2{X: values[0], Y: values[1]})
+	}
+
+	for _, row := range input[emptyLineId+1:] {
+		values := utils.ToIntSlice(strings.Split(row, ","))
+		valid := true
+
+		for i, second := range values {
+			for _, first := range values[i+1:] {
+				if slices.Contains(constraints, types.Vec2{X: first, Y: second}) {
+					valid = false
+				}
+			}
+
+		}
+		if valid {
+			sum += values[(len(values)-1)/2]
+		}
+	}
+
+	return strconv.Itoa(sum)
 }
 
 // Part2 solves the second part of the exercise
