c0301137b0826d1db301e9816b318b8ab98e70f0
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-12-06 09:35:15 +0100

    part 1

--- days/day_06/main.go
+++ days/day_06/main.go
@@ -2,6 +2,10 @@ package day_06
 
 import (
 	"fmt"
+	"github.com/wlchs/aoc24/types"
+	"github.com/wlchs/aoc24/utils"
+	"slices"
+	"strconv"
 )
 
 // Run function of the daily challenge
@@ -16,7 +20,32 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	m := utils.ParseInputToMap(input)
+	var guardPosition types.Vec2
+	guardDir := types.Vec2{X: 0, Y: -1}
+
+	for pos, char := range m {
+		if char == '^' {
+			guardPosition = pos
+			break
+		}
+	}
+
+	var visitedLocations []types.Vec2
+	for guardPosition.X >= 0 && guardPosition.X < len(input[0]) && guardPosition.Y >= 0 && guardPosition.Y < len(input) {
+		if !slices.Contains(visitedLocations, guardPosition) {
+			visitedLocations = append(visitedLocations, guardPosition)
+		}
+
+		nextPosition := guardPosition.Add(&guardDir)
+		if m[nextPosition] != '#' {
+			guardPosition = nextPosition
+		} else {
+			guardDir = guardDir.RotateRight()
+		}
+	}
+
+	return strconv.Itoa(len(visitedLocations))
 }
 
 // Part2 solves the second part of the exercise
