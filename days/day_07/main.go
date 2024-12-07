743bc6bcd86424768db6844d127c173cd4a2ef50
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-12-07 23:15:47 +0100

    part 1

--- days/day_07/main.go
+++ days/day_07/main.go
@@ -2,7 +2,19 @@ package day_07
 
 import (
 	"fmt"
+	"github.com/wlchs/aoc24/utils"
+	"slices"
+	"strconv"
+	"strings"
 )
+
+// valid checks if the desired result can be achieved using various operators between the given values
+func valid(result float64, values []float64) bool {
+	if len(values) == 0 {
+		return result == 0
+	}
+	return valid(result-values[0], values[1:]) || valid(result/values[0], values[1:])
+}
 
 // Run function of the daily challenge
 func Run(input []string, mode int) {
@@ -16,7 +28,20 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	sum := 0
+
+	for _, line := range input {
+		split := strings.Split(line, ": ")
+		result, _ := strconv.ParseFloat(split[0], 64)
+		values := utils.ToFloatSlice(strings.Split(split[1], " "))
+
+		slices.Reverse(values)
+
+		if valid(result, values) {
+			sum += int(result)
+		}
+	}
+	return strconv.Itoa(sum)
 }
 
 // Part2 solves the second part of the exercise
--- utils/slices.go
+++ utils/slices.go
@@ -11,6 +11,16 @@ func ToIntSlice(numbers []string) []int {
 	return s
 }
 
+// ToFloatSlice converts a string slice to a float64 slice
+func ToFloatSlice(numbers []string) []float64 {
+	s := make([]float64, 0, len(numbers))
+	for _, number := range numbers {
+		f, _ := strconv.ParseFloat(number, 64)
+		s = append(s, f)
+	}
+	return s
+}
+
 // ToStringSlice converts an int slice to a string slice
 func ToStringSlice(numbers []int) []string {
 	s := make([]string, 0, len(numbers))
