4832e35fa2db610f23e86a1a34a49c70e6b42106
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-12-12 08:48:55 +0100

    part 1

--- days/day_12/main.go
+++ days/day_12/main.go
@@ -2,7 +2,61 @@ package day_12
 
 import (
 	"fmt"
+	"github.com/wlchs/aoc24/types"
+	"github.com/wlchs/aoc24/utils"
+	"slices"
+	"strconv"
 )
+
+// regionKey is an alias for int32, just to make the code more readable
+type regionKey = int32
+
+// regionData contains the area and perimeter of a fields
+type regionData struct {
+	area      uint64
+	perimeter uint64
+}
+
+// regionCtx holds the calculation context
+type regionCtx struct {
+	fields    []types.Vec2
+	perimeter uint64
+}
+
+// calculate calculates the area and perimeter of each regionData
+func calculate(m map[types.Vec2]regionKey) []regionData {
+	var res []regionData
+	regions := map[regionKey][][]types.Vec2{}
+	processed := make([]types.Vec2, 0, len(m))
+	for pos, key := range m {
+		if slices.Contains(processed, pos) {
+			continue
+		}
+		var ctx regionCtx
+		find(m, pos, key, &ctx)
+		if _, ok := regions[key]; !ok {
+			regions[key] = [][]types.Vec2{}
+		}
+		regions[key] = append(regions[key], ctx.fields)
+		processed = append(processed, ctx.fields...)
+		res = append(res, regionData{uint64(len(ctx.fields)), ctx.perimeter})
+	}
+	return res
+}
+
+// find executes a recursive search starting from pos in m and fills the fields with similarly marked neighbouring coordinates
+func find(m map[types.Vec2]regionKey, pos types.Vec2, key regionKey, ctx *regionCtx) {
+	if m[pos] != key {
+		ctx.perimeter++
+		return
+	} else if slices.Contains(ctx.fields, pos) {
+		return
+	}
+	ctx.fields = append(ctx.fields, pos)
+	for _, nextPos := range pos.Around() {
+		find(m, nextPos, key, ctx)
+	}
+}
 
 // Run function of the daily challenge
 func Run(input []string, mode int) {
@@ -16,7 +70,12 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	m := utils.ParseInputToMap(input)
+	sum := uint64(0)
+	for _, r := range calculate(m) {
+		sum += r.area * r.perimeter
+	}
+	return strconv.FormatUint(sum, 10)
 }
 
 // Part2 solves the second part of the exercise
