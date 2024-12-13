e4c4fb52f8626325ac60b627dafe93ec5798d09d
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-12-13 11:06:39 +0100

    part 1

--- days/day_13/main.go
+++ days/day_13/main.go
@@ -2,7 +2,50 @@ package day_13
 
 import (
 	"fmt"
+	"github.com/wlchs/aoc24/types"
+	"github.com/wlchs/aoc24/utils"
+	"regexp"
+	"strconv"
 )
+
+// scenario describes a test case
+type scenario struct {
+	A, B, X types.Vec2
+}
+
+// parseInput receives the input and creates a scenario slice from it
+func parseInput(input []string) []scenario {
+	res := make([]scenario, 0, (len(input)+1)/4)
+	cur := scenario{}
+	re := regexp.MustCompile(".*X=?(?P<X>.*), Y=?(?P<Y>.*)")
+	for i, s := range input {
+		match := re.FindStringSubmatch(s)
+		switch i % 4 {
+		case 0:
+			cur.A = types.Vec2{X: utils.Atoi(match[1]), Y: utils.Atoi(match[2])}
+		case 1:
+			cur.B = types.Vec2{X: utils.Atoi(match[1]), Y: utils.Atoi(match[2])}
+		case 2:
+			cur.X = types.Vec2{X: utils.Atoi(match[1]), Y: utils.Atoi(match[2])}
+			res = append(res, cur)
+		}
+	}
+	return res
+}
+
+// minCost calculates the minimum cost of tickets required to get the price if possible
+func minCost(s scenario) int {
+	for b := range 100 {
+		for a := range 100 {
+			posA := s.A.Multiply(a)
+			posB := s.B.Multiply(b)
+			if posA.Add(&posB) == s.X {
+				return 3*a + b
+			}
+		}
+	}
+	return 0
+}
 
 // Run function of the daily challenge
 func Run(input []string, mode int) {
@@ -16,7 +59,12 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	scenarios := parseInput(input)
+	cost := 0
+	for _, s := range scenarios {
+		cost += minCost(s)
+	}
+	return strconv.Itoa(cost)
 }
 
 // Part2 solves the second part of the exercise
