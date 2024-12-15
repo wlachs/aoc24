7b66da2f29c741bacdcb787f692ef721ce332b85
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-12-15 23:39:16 +0100

    part 2

--- days/day_14/main.go
+++ days/day_14/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/wlchs/aoc24/types"
 	"github.com/wlchs/aoc24/utils"
 	"regexp"
+	"slices"
 	"strconv"
 )
 
@@ -15,12 +16,18 @@ type robot struct {
 
 // quadrant returns the ID of the quadrant where the robot current is
 // If the robot is exactly on the border between two quadrants, return false
-func (r robot) quadrant(width, height int) (int, bool) {
+func (r *robot) quadrant(width, height int) (int, bool) {
 	w, h := width/2, height/2
 	if r.p.X == w || r.p.Y == h {
 		return -1, false
 	}
 	return 2*(r.p.X/(w+1)) + r.p.Y/(h+1), true
+}
+
+// move simulates a robot movement
+func (r *robot) move(width, height int) {
+	r.p.X = utils.Mod(r.p.X+r.v.X, width)
+	r.p.Y = utils.Mod(r.p.Y+r.v.Y, height)
 }
 
 // parseRobots parses the input and returns a slice of robots
@@ -36,63 +43,75 @@ func parseRobots(input []string) []robot {
 	return robots
 }
 
+// hasNLargeConnectedSubGraph searches the robot graph for an n large connected subgraph
+func hasNLargeConnectedSubGraph(robots []robot, n int) bool {
+	for _, r := range robots {
+		visitedElements := make([]types.Vec2, 0, n)
+		elementsToVisit := []types.Vec2{r.p}
+		for len(elementsToVisit) > 0 {
+			for _, vec := range elementsToVisit[0].Around() {
+				if checkRobotAt(robots, vec) && !slices.Contains(visitedElements, vec) {
+					elementsToVisit = append(elementsToVisit, vec)
+				}
+			}
+			visitedElements = append(visitedElements, elementsToVisit[0])
+			elementsToVisit = elementsToVisit[1:]
+			if len(visitedElements) >= n {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// checkRobotAt checks if there is a robot at the given vector
+func checkRobotAt(robots []robot, vec types.Vec2) bool {
+	for _, r := range robots {
+		if r.p == vec {
+			return true
+		}
+	}
+	return false
+}
+
 // Run function of the daily challenge
 func Run(input []string, mode int) {
 	if mode == 1 || mode == 3 {
 		fmt.Printf("Part one: %v\n", Part1(input, 101, 103))
 	}
 	if mode == 2 || mode == 3 {
-		fmt.Printf("Part two: %v\n", Part2(input))
+		fmt.Printf("Part two: %v\n", Part2(input, 101, 103))
 	}
 }
 
 // Part1 solves the first part of the exercise
 func Part1(input []string, width, height int) string {
 	robots := parseRobots(input)
-
-	// printRobots(robots, width, height)
-	for range 100 {
-		for i, r := range robots {
-			robots[i].p = types.Vec2{X: utils.Mod(r.p.X+r.v.X, width), Y: utils.Mod(r.p.Y+r.v.Y, height)}
-		}
-		//printRobots(robots, width, height)
-	}
 	quadrants := make([]uint64, 4)
+	for range 100 {
+		for i := range robots {
+			robots[i].move(width, height)
+		}
+	}
 	for _, r := range robots {
 		if q, ok := r.quadrant(width, height); ok {
 			quadrants[q]++
 		}
 	}
-	for _, r := range robots {
-		q, ok := r.quadrant(width, height)
-		fmt.Printf("%v -> %d(%v)\n", r, q, ok)
-	}
-	for i, quadrant := range quadrants {
-		fmt.Printf("%d->%d\n", i, quadrant)
-	}
 	return strconv.FormatUint(quadrants[0]*quadrants[1]*quadrants[2]*quadrants[3], 10)
 }
 
-func printRobots(robots []robot, width, height int) {
-	m := map[types.Vec2]int{}
-	for _, r := range robots {
-		m[r.p]++
-	}
-	for y := range height {
-		for x := range width {
-			i, ok := m[types.Vec2{X: x, Y: y}]
-			if ok {
-				fmt.Print(i)
-			} else {
-				fmt.Print(".")
-			}
-		}
-		fmt.Println()
-	}
-	fmt.Println()
-}
-
 // Part2 solves the second part of the exercise
-func Part2(input []string) string {
-	return ""
+func Part2(input []string, width, height int) string {
+	robots := parseRobots(input)
+	round := 0
+	for {
+		if hasNLargeConnectedSubGraph(robots, 30) {
+			return strconv.Itoa(round)
+		}
+		for i := range robots {
+			robots[i].move(width, height)
+		}
+		round++
+	}
 }
