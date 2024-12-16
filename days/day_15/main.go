59364b24c32f7e21f0cb836ea4bc3966a4a47a74
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-12-16 11:54:01 +0100

    part 2

--- days/day_15/main.go
+++ days/day_15/main.go
@@ -49,12 +49,34 @@ func robot(m map[types.Vec2]int32) types.Vec2 {
 }
 
 // push tries to recursively push the item at the given position in the given direction
-func push(m map[types.Vec2]int32, pos types.Vec2, dir *types.Vec2) bool {
+func push(m map[types.Vec2]int32, pos types.Vec2, dir *types.Vec2, save bool) bool {
+	cascade := func(side types.Vec2) bool {
+		if push(m, pos.Add(dir), dir, false) && push(m, pos.Add(dir).Add(&side), dir, false) {
+			if save {
+				push(m, pos.Add(dir), dir, save)
+				push(m, pos.Add(dir).Add(&side), dir, save)
+				m[pos.Add(dir)], m[pos.Add(dir).Add(&side)] = m[pos], m[pos.Add(&side)]
+				m[pos], m[pos.Add(&side)] = '.', '.'
+			}
+			return true
+		}
+		return false
+	}
 	switch m[pos] {
+	case '[':
+		if dir.Y != 0 {
+			return cascade(RIGHT)
+		}
+		fallthrough
+	case ']':
+		if dir.Y != 0 {
+			return cascade(LEFT)
+		}
+		fallthrough
 	case '@':
 		fallthrough
 	case 'O':
-		if push(m, pos.Add(dir), dir) {
+		if push(m, pos.Add(dir), dir, save) {
 			m[pos.Add(dir)] = m[pos]
 			m[pos] = '.'
 			return true
@@ -74,11 +96,27 @@ func gps(vec2 types.Vec2) int {
 func sum(m map[types.Vec2]int32) int {
 	s := 0
 	for vec2, c := range m {
-		if c == 'O' {
+		if c == 'O' || c == '[' {
 			s += gps(vec2)
 		}
 	}
 	return s
+}
+
+// extend modifies the input map by doubling the width of every element other than '@', then returns the new map
+func extend(m map[types.Vec2]int32) map[types.Vec2]int32 {
+	extended := map[types.Vec2]int32{}
+	for vec2, c := range m {
+		switch c {
+		case '@':
+			extended[types.Vec2{X: vec2.X * 2, Y: vec2.Y}], extended[types.Vec2{X: vec2.X*2 + 1, Y: vec2.Y}] = c, '.'
+		case 'O':
+			extended[types.Vec2{X: vec2.X * 2, Y: vec2.Y}], extended[types.Vec2{X: vec2.X*2 + 1, Y: vec2.Y}] = '[', ']'
+		default:
+			extended[types.Vec2{X: vec2.X * 2, Y: vec2.Y}], extended[types.Vec2{X: vec2.X*2 + 1, Y: vec2.Y}] = c, c
+		}
+	}
+	return extended
 }
 
 // Run function of the daily challenge
@@ -94,29 +132,20 @@ func Run(input []string, mode int) {
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
 	m, instructions := readMapAndInstructions(input)
-	// fmt.Println(m)
-	// fmt.Println(sizes)
-	// fmt.Println(instructions)
-	// printMap(m, sizes)
 	for _, instruction := range instructions {
 		pos := robot(m)
-		// fmt.Println(instruction)
-		push(m, pos, &instruction)
-		// printMap(m, sizes)
+		push(m, pos, &instruction, true)
 	}
 	return strconv.Itoa(sum(m))
 }
 
-func printMap(m map[types.Vec2]int32, sizes types.Vec2) {
-	for y := range sizes.Y {
-		for x := range sizes.X {
-			fmt.Print(string(m[types.Vec2{X: x, Y: y}]))
-		}
-		fmt.Println()
-	}
-}
-
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	m, instructions := readMapAndInstructions(input)
+	m = extend(m)
+	for _, instruction := range instructions {
+		pos := robot(m)
+		push(m, pos, &instruction, true)
+	}
+	return strconv.Itoa(sum(m))
 }
