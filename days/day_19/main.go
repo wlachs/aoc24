9e00dca1b23ea791109b027ac460e1c4286dba6b
Laszlo Borbely <laszlo.borbely@mgm-tp.com>
2024-12-19 13:57:08 +0100

    part 1

--- days/day_19/main.go
+++ days/day_19/main.go
@@ -2,7 +2,22 @@ package day_19
 
 import (
 	"fmt"
+	"slices"
+	"strconv"
+	"strings"
 )
+
+func f(words []string, s string) bool {
+	if s == "" {
+		return true
+	}
+	for i := len(s); i > 0; i-- {
+		if slices.Contains(words, s[:i]) && f(words, s[i:]) {
+			return true
+		}
+	}
+	return false
+}
 
 // Run function of the daily challenge
 func Run(input []string, mode int) {
@@ -16,7 +31,14 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	words := strings.Split(input[0], ", ")
+	count := 0
+	for _, s := range input[2:] {
+		if f(words, s) {
+			count++
+		}
+	}
+	return strconv.Itoa(count)
 }
 
 // Part2 solves the second part of the exercise
