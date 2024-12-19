186cbfd623c4c3850d465e1a0434c36f198eb2b5
László Borbély <laszlo.borbely@mgm-tp.com>
2024-12-19 14:28:52 +0100

    Day 19 (#20)
    * part 1
    
    * part 2

--- days/day_19/main.go
+++ days/day_19/main.go
@@ -2,7 +2,24 @@ package day_19
 
 import (
 	"fmt"
+	"slices"
+	"strconv"
+	"strings"
 )
+
+// f counts all possible ways to combine words to reach the desired outcome
+func f(words []string, s string, m map[string]int) bool {
+	if _, ok := m[s]; ok {
+		return true
+	}
+	for i := len(s); i > 0; i-- {
+		if slices.Contains(words, s[:i]) && f(words, s[i:], m) {
+			m[s] += m[s[i:]]
+		}
+	}
+	_, ok := m[s]
+	return ok
+}
 
 // Run function of the daily challenge
 func Run(input []string, mode int) {
@@ -16,10 +33,25 @@ func Run(input []string, mode int) {
 
 // Part1 solves the first part of the exercise
 func Part1(input []string) string {
-	return ""
+	words := strings.Split(input[0], ", ")
+	count := 0
+	m := map[string]int{"": 1}
+	for _, s := range input[2:] {
+		if f(words, s, m) {
+			count++
+		}
+	}
+	return strconv.Itoa(count)
 }
 
 // Part2 solves the second part of the exercise
 func Part2(input []string) string {
-	return ""
+	words := strings.Split(input[0], ", ")
+	count := 0
+	m := map[string]int{"": 1}
+	for _, s := range input[2:] {
+		f(words, s, m)
+		count += m[s]
+	}
+	return strconv.Itoa(count)
 }
